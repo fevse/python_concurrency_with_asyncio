04b8d56f09b489ff45a577b8019afa4b32b51263
fevse <fedulov.v.s@gmail.com>
2023-04-12 19:59:59 +0600

    add new content

---
+++ listing_2_17.py
@@ -0,0 +1,12 @@
+import asyncio
+from util import delay, async_timed
+
+@async_timed()
+async def main():
+    task_one = asyncio.create_task(delay(2))
+    task_two = asyncio.create_task(delay(3))
+
+    await task_one
+    await task_two
+
+asyncio.run(main())
