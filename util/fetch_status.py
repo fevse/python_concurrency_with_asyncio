b3903c53bfb03d3dacefcd050593e23b5d1e6139
fevse <fedulov.v.s@gmail.com>
2023-04-24 17:00:08 +0600

    add fetch_status function

---
+++ util/fetch_status.py
@@ -0,0 +1,10 @@
+import asyncio
+import aiohttp
+from aiohttp import ClientSession
+from util import async_timed
+
+@async_timed()
+async def fetch_status(session: ClientSession, url: str) -> int:
+    async with session.get(url) as result:
+        return result.status
+
